000b5facf5eaeaf6710a9609fd40e9c68f0b6e46
xmyhhh <xmycissy@outlook.com>
2022-04-27 21:46:21 +0800

    update

--- Yield_2/Program.cs
+++ Yield_2/Program.cs
@@ -41,10 +41,49 @@ public class Program
 {
     static void Main()
     {
-        var primeNumber = Gennerator.GetPrimeNumbers().Take(100);//要多少，生成多少
-        foreach(int prime in primeNumber)
+        var primeNumber0 = Gennerator.GetPrimeNumbers().Take(10).ToList(); //Take返回一个可迭代对象子对象转换成list
+
+
+
+        var primeNumber1 = Gennerator.GetPrimeNumbers().Take(10);
+        foreach (int prime in primeNumber1)
         {
             Console.WriteLine(prime);
         }
+        Console.WriteLine("-------Gennerator.GetPrimeNumbers().Take(10)--------");
+
+        var primeNumber2 = Gennerator.GetPrimeNumbers().Take(10);//take会重头开始
+        foreach (int prime in primeNumber2)
+        {
+            Console.WriteLine(prime);
+        }
+
+        Console.WriteLine("-------Gennerator.GetPrimeNumbers().Take(10)--------");
+        var iterator = Gennerator.GetPrimeNumbers().GetEnumerator();
+
+        for (int i = 0; i < 10; i++)
+        {
+            if (iterator.MoveNext())
+            {
+                Console.WriteLine(iterator.Current);
+            }
+            else
+            {
+                break;  //如果可迭代对象没有下一个，就退出
+            }
+        }
+        Console.WriteLine("-------Gennerator.GetPrimeNumbers().GetEnumerator();--------");
+        for (int i = 0; i < 10; i++)
+        {
+            if (iterator.MoveNext())
+            {
+                Console.WriteLine(iterator.Current);
+            }
+            else
+            {
+                break;  //如果可迭代对象没有下一个，就退出
+            }
+        }
+        Console.WriteLine("-------Gennerator.GetPrimeNumbers().GetEnumerator();--------");
     }
 }
