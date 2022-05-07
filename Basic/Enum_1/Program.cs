1e25f72c748fe81613238b5c21ee1dbbbdf861be
xmy <xmycissy@outlook.com>
2022-05-07 16:57:54 +0800

    update

---
+++ Basic/Enum_1/Program.cs
@@ -0,0 +1,24 @@
+﻿public enum DamageType
+{
+    Physical,
+    Fire,
+    Cold,
+    Electric
+
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        var a = Enum.GetValues(typeof(DamageType)); //检索指定枚举中常数值的数组
+        var a_len = a.Length; //获取数组长度
+        Console.WriteLine(a);
+
+        foreach (int i in Enum.GetValues(typeof(DamageType)))
+            Console.WriteLine(i);
+    }
+
+    //int[] elementalProtection = new int[Enum.GetValues(typeof(DamageType)).Length];
+}
+
---
+++ Keywords/new_1/Program.cs
@@ -0,0 +1,39 @@
+﻿public class Program
+{
+    static void Main()
+    {
+        MyClass_2 myClass = new MyClass_2();
+        Console.WriteLine(myClass.stats.a);
+
+        myClass.stats.a = 1;
+        Console.WriteLine(myClass.stats.a);
+    }
+
+
+}
+
+public class MyClass_1
+{
+    public Stats stats { get; set; } = new Stats();
+}
+public class MyClass_2
+{
+    public Stats stats
+    {
+        get
+        {
+            return m_stats;
+        }
+        set
+        {
+            m_stats = value;
+        }
+    }
+
+    Stats m_stats = new Stats();
+}
+
+public class Stats
+{
+    public int a = 0;
+}
